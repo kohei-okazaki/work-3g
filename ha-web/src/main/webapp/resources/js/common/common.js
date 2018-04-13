56d2314e8ab2eee564ce19691ea16d7cfbcded11
kohei-okazaki <kou1210hei@gmail.com>
2018-04-14 00:15:49 +0900

    修正

--- ha-web/src/main/webapp/resources/js/common/common.js
+++ ha-web/src/main/webapp/resources/js/common/common.js
@@ -5,7 +5,7 @@ function finish() {
 
 function toMenu() {
 	var form = document.createElement('form');
-	form.action = '/isol-web/menu.html';
+	form.action = '/ha-web/menu.html';
 	form.method = 'get';
 	document.body.appendChild(form);
 	form.submit();
