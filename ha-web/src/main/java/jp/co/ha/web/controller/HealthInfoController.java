81df7fee4d8e5bee245dbeecd94e517076bad0a3
kohei-okazaki <kou1210hei@gmail.com>
2018-04-14 18:59:13 +0900

    Jsonutil追加 pomにjackson 追加

--- ha-web/src/main/java/jp/co/ha/web/controller/HealthInfoController.java
+++ ha-web/src/main/java/jp/co/ha/web/controller/HealthInfoController.java
@@ -55,6 +55,7 @@ public class HealthInfoController implements BaseWizardController<HealthInfoForm
 	@Autowired
 	@HealthInfoCsv
 	private CsvDownloadService csvDownloadService;
+
 	/**
 	 * {@inheritDoc}
 	 */
