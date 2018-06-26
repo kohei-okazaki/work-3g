a043f8cbf5ae019f13d5769bf2cbefa28679ccee
kohei-okazaki <kou1210hei@gmail.com>
2018-06-26 22:37:21 +0900

    修正

---
+++ ha-common/src/main/java/jp/co/ha/common/log/AppLogger.java
@@ -0,0 +1,15 @@
+package jp.co.ha.common.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppLogger {
+
+	/** ロガー */
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+	public void info(Class<?> clazz, Object object) {
+
+	}
+
+}
