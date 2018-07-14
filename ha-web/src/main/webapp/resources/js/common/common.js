4f1da723b1784941d9b8cd1d01ed68952f894ba8
kohei-okazaki <kou1210hei@gmail.com>
2018-07-14 16:12:33 +0900

    修正

--- ha-web/src/main/webapp/resources/js/common/common.js
+++ ha-web/src/main/webapp/resources/js/common/common.js
@@ -3,10 +3,3 @@ function finish() {
 	alert(finishMessage);
 }
 
-function toMenu() {
-	var form = document.createElement('form');
-	form.action = '/ha-web/menu.html';
-	form.method = 'get';
-	document.body.appendChild(form);
-	form.submit();
-}
