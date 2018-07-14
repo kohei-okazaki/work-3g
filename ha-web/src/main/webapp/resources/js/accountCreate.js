ff68409096849b4602892365dfcda7acc92f91dc
kohei-okazaki <kou1210hei@gmail.com>
2018-07-14 16:07:37 +0900

    進捗バー作成

--- ha-web/src/main/webapp/resources/js/accountCreate.js
+++ ha-web/src/main/webapp/resources/js/accountCreate.js
@@ -6,3 +6,29 @@ function accountCreate() {
 	form.submit();
 }
 
+function changeValue() {
+
+	// 入力値を取得
+	var userIdTag = document.getElementById("userId");
+	var userIdValue = userIdTag.value;
+	var passwordTag = document.getElementById("password");
+	var passwordValue = passwordTag.value;
+	var confirmPasswordTag = document.getElementById("confirmPassword");
+	var confirmPasswordValue = confirmPasswordTag.value;
+
+	// 進捗バーを取得
+	var bar = document.getElementById("bar");
+	var userIdVal = 0;
+	var passwordVal = 0;
+	var confirmPasswordVal = 0;
+	if (userIdValue !== "") {
+		userIdVal = 1;
+	}
+	if (passwordValue !== "") {
+		passwordVal = 1;
+	}
+	if (confirmPasswordValue !== "") {
+		confirmPasswordVal = 1;
+	}
+	bar.value = userIdVal + passwordVal + confirmPasswordVal;
+}
