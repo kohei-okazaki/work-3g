df8a30542c3ba52ffed1ae794a53c2dde665f962
kohei-okazaki <kou1210hei@gmail.com>
2018-08-02 22:38:37 +0900

    no message

--- ha-build/bat/maven-build.bat
+++ ha-build/bat/maven-build.bat
@@ -7,10 +7,10 @@ rem jar
 set ver=1.0
 
 cd %baseDir%\ha-common
-call mvn install:install-file -Dfile=target/ha-common-1.0.jar -DgroupId=jp.co.ha.common -DartifactId=ha-common -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=target/ha-common-%ver%.jar -DgroupId=jp.co.ha.common -DartifactId=ha-common -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true
 
 cd %baseDir%\ha-business
-call mvn install:install-file -Dfile=target/ha-business-1.0.jar -DgroupId=jp.co.ha.business -DartifactId=ha-business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=target/ha-business-%ver%.jar -DgroupId=jp.co.ha.business -DartifactId=ha-business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true
 
 
 cd %~dp0
