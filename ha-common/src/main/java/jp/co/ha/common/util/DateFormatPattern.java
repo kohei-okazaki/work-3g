d8d8b5e0a5de0b29e168c2bf0b8694af874d4057
kohei-okazaki <kou1210hei@gmail.com>
2018-08-05 13:14:38 +0900

    no message

--- ha-common/src/main/java/jp/co/ha/common/util/DateFormatPattern.java
+++ ha-common/src/main/java/jp/co/ha/common/util/DateFormatPattern.java
@@ -9,24 +9,12 @@ public enum DateFormatPattern {
 	YYYYMMDD("yyyy/MM/dd"),
 	YYYYMMDD_HHMMSS("yyyy/MM/dd HH:mm:ss");
 
-	/** フォーマット名 */
 	private String value;
 
-	/**
-	 * コンストラクタ<br>
-	 *
-	 * @param value
-	 *     フォーマット名
-	 */
 	private DateFormatPattern(String value) {
 		this.value = value;
 	}
 
-	/**
-	 * フォーマット名を返す<br>
-	 *
-	 * @return フォーマット名
-	 */
 	public String getValue() {
 		return this.value;
 	}
