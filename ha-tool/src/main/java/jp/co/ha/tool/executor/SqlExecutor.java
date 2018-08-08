3f20c21693e0315bbe2aaf2d36eed937342d9df4
kohei-okazaki <kou1210hei@gmail.com>
2018-08-08 17:19:42 +0900

    no message

--- ha-tool/src/main/java/jp/co/ha/tool/executor/SqlExecutor.java
+++ ha-tool/src/main/java/jp/co/ha/tool/executor/SqlExecutor.java
@@ -1,12 +1,18 @@
 package jp.co.ha.tool.executor;
 
 import jp.co.ha.tool.build.DdlBuilder;
+import jp.co.ha.tool.build.EntityBuilder;
 
 public class SqlExecutor {
 
 	public static void main(String[] args) {
-		DdlBuilder builder = new DdlBuilder();
-		builder.execute();
+		DdlBuilder ddlBuilder = new DdlBuilder();
+		// excelファイルからDDLを作成
+		ddlBuilder.execute();
+		// DDLをファイルに書き出す
+//		ddlBuilder.comit();
+
+		EntityBuilder entityBuilder = new EntityBuilder();
 
 
 	}
