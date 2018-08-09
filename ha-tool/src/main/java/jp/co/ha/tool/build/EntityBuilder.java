502c6e6d5931b87126c1ab511e63cb95f1169149
kohei-okazaki <kou1210hei@gmail.com>
2018-08-10 01:23:11 +0900

    no message

--- ha-tool/src/main/java/jp/co/ha/tool/build/EntityBuilder.java
+++ ha-tool/src/main/java/jp/co/ha/tool/build/EntityBuilder.java
@@ -1,15 +1,9 @@
 package jp.co.ha.tool.build;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import jp.co.ha.tool.reader.PropertyReader;
 
 public class EntityBuilder {
 
@@ -17,20 +11,6 @@ public class EntityBuilder {
 
 	/** Tableリスト */
 	private List<String> tableList;
-
-	public EntityBuilder() {
-		this.init();
-	}
-
-	private void init() {
-
-		Properties prop = new PropertyReader().getProperty("target.properties");
-		String target = prop.getProperty("target");
-		if (Objects.nonNull(target)) {
-			this.tableList = Stream.of(target.split(",")).collect(Collectors.toList());
-		}
-
-	}
 
 	public void execute() {
 
