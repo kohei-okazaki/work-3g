a6d63916a3afe3b0cd628e46a88a43e7321608d0
kohei-okazaki <kou1210hei@gmail.com>
2018-08-11 21:42:56 +0900

    no message

--- ha-web/src/main/webapp/resources/js/common/common.js
+++ ha-web/src/main/webapp/resources/js/common/common.js
@@ -10,3 +10,11 @@ function toTop() {
 	document.body.appendChild(form);
 	form.submit();
 }
+
+$("form").submit(function() {
+	var self = this;
+	$(":submit", self).prop("disabled", true);
+	setTimeout(function() {
+		$(":submit", self).prop("disabled", false);
+	}, 10000);
+});
