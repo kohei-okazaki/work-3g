71b46b3f42f81e4d835099bb353a8e0ae52251c9
kohei-okazaki <kou1210hei@gmail.com>
2018-08-26 14:07:59 +0900

    no message

--- ha-tool/bat/maven-clean.bat
+++ ha-tool/bat/maven-clean.bat
@@ -1,15 +1,24 @@
 @echo off
 
 rem iniファイルを読み込む
+echo START initialize.bat
 call initialize.bat
+echo END initialize.bat
 
-rem targetディレクトリをクリーン
+rem common/targetディレクトリをクリーン
+echo START common project clean
 cd %baseDir%\ha-common\target
 call mvn clean package
 call mvn clean install
+echo END common project clean
 
+
+rem business/targetディレクトリをクリーン
+echo START business project clean
 cd %baseDir%\ha-business\target
 call mvn clean package
 call mvn clean install
+echo END business project clean
+
 
 cd %~dp0
