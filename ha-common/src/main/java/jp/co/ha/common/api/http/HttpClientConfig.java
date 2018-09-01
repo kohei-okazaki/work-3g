af970a30a7e25ae7048264b1629b41214c0c2f50
kohei-okazaki <kou1210hei@gmail.com>
2018-09-02 00:44:24 +0900

    no message

---
+++ ha-common/src/main/java/jp/co/ha/common/api/http/HttpClientConfig.java
@@ -0,0 +1,70 @@
+package jp.co.ha.common.api.http;
+
+/**
+ * HTTPクライアントの設定情報保持クラス
+ *
+ */
+public class HttpClientConfig {
+
+	/** リクエストURL */
+	private String requestUrl;
+	/** タイムアウトまでの時間 */
+	private int timeout;
+	/** HTTPメソッド */
+	private HttpMethod httpMethod;
+
+	/**
+	 * requestUrlを返す<br>
+	 *
+	 * @return requestUrl
+	 */
+	public String getRequestUrl() {
+		return requestUrl;
+	}
+
+	/**
+	 * requestUrlを設定する<br>
+	 *
+	 * @param requestUrl
+	 */
+	public void setRequestUrl(String requestUrl) {
+		this.requestUrl = requestUrl;
+	}
+
+	/**
+	 * timeoutを返す<br>
+	 *
+	 * @return timeout
+	 */
+	public int getTimeout() {
+		return timeout;
+	}
+
+	/**
+	 * timeoutを設定する<br>
+	 *
+	 * @param timeout
+	 */
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+
+	/**
+	 * httpMethodを返す<br>
+	 *
+	 * @return httpMethod
+	 */
+	public HttpMethod getHttpMethod() {
+		return httpMethod;
+	}
+
+	/**
+	 * httpMethodを設定する<br>
+	 *
+	 * @param httpMethod
+	 */
+	public void setHttpMethod(HttpMethod httpMethod) {
+		this.httpMethod = httpMethod;
+	}
+
+}
