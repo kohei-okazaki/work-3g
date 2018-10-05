704904d79d2ec91545c39b809614cb2033189990
kohei-okazaki <kou1210hei@gmail.com>
2018-10-05 22:15:23 +0900

    no message

---
+++ ha-business/src/main/java/jp/co/ha/business/api/response/CommonResponse.java
@@ -0,0 +1,11 @@
+package jp.co.ha.business.api.response;
+
+import jp.co.ha.common.api.response.BaseResponse;
+
+/**
+ * 共通レスポンス情報保持クラス<br>
+ *
+ */
+public abstract class CommonResponse extends BaseResponse {
+
+}
