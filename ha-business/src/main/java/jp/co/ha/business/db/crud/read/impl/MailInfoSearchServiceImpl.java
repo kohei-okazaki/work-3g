97af3d5ee277e02fa0e5394911f2f1b3ed9be104
kohei-okazaki <kou1210hei@gmail.com>
2018-11-03 11:10:03 +0900

    no message

--- ha-business/src/main/java/jp/co/ha/business/db/crud/read/impl/MailInfoSearchServiceImpl.java
+++ ha-business/src/main/java/jp/co/ha/business/db/crud/read/impl/MailInfoSearchServiceImpl.java
@@ -2,8 +2,8 @@ package jp.co.ha.business.db.crud.read.impl;
 
 import org.apache.ibatis.session.SqlSession;
 
+import jp.co.ha.business.db.SqlSessionFactory;
 import jp.co.ha.business.db.crud.read.MailInfoSearchService;
-import jp.co.ha.business.db.dao.MyBatisDao;
 import jp.co.ha.common.exception.BaseException;
 import jp.co.ha.db.entity.MailInfo;
 import jp.co.ha.db.mapper.MailInfoMapper;
@@ -12,14 +12,14 @@ import jp.co.ha.db.mapper.MailInfoMapper;
  * メール情報検索サービスインターフェース実装クラス<br>
  *
  */
-public class MailInfoSearchServiceImpl implements MailInfoSearchService, MyBatisDao {
+public class MailInfoSearchServiceImpl implements MailInfoSearchService {
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public MailInfo findByUserId(String userId) throws BaseException {
-		try (SqlSession session = getSqlSession()) {
+		try (SqlSession session = SqlSessionFactory.getInstance().getSqlSession()) {
 			MailInfoMapper mapper = session.getMapper(MailInfoMapper.class);
 			return mapper.selectByPrimaryKey(userId);
 		}
