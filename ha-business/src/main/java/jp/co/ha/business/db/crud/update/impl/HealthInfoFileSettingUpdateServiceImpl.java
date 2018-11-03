daf0daa40b85043b9f2f788fcc4bcd85d54e1fa3
kohei-okazaki <kou1210hei@gmail.com>
2018-11-03 11:00:57 +0900

    修正

--- ha-business/src/main/java/jp/co/ha/business/db/crud/update/impl/HealthInfoFileSettingUpdateServiceImpl.java
+++ ha-business/src/main/java/jp/co/ha/business/db/crud/update/impl/HealthInfoFileSettingUpdateServiceImpl.java
@@ -2,8 +2,8 @@ package jp.co.ha.business.db.crud.update.impl;
 
 import org.apache.ibatis.session.SqlSession;
 
+import jp.co.ha.business.db.SqlSessionFactory;
 import jp.co.ha.business.db.crud.update.HealthInfoFileSettingUpdateService;
-import jp.co.ha.business.db.dao.MyBatisDao;
 import jp.co.ha.common.exception.BaseException;
 import jp.co.ha.db.entity.HealthInfoFileSetting;
 import jp.co.ha.db.mapper.HealthInfoFileSettingMapper;
@@ -12,14 +12,14 @@ import jp.co.ha.db.mapper.HealthInfoFileSettingMapper;
  * 健康情報ファイル設定更新サービス実装クラス
  *
  */
-public class HealthInfoFileSettingUpdateServiceImpl implements HealthInfoFileSettingUpdateService, MyBatisDao {
+public class HealthInfoFileSettingUpdateServiceImpl implements HealthInfoFileSettingUpdateService {
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void update(HealthInfoFileSetting entity) throws BaseException {
-		try (SqlSession session = getSqlSession()) {
+		try (SqlSession session = SqlSessionFactory.getInstance().getSqlSession()) {
 			HealthInfoFileSettingMapper mapper = session.getMapper(HealthInfoFileSettingMapper.class);
 			mapper.updateByPrimaryKey(entity);
 			session.commit();
