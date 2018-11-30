4a8644968001f9169bfe37f84b2f900301328449
kohei-okazaki <kou1210hei@gmail.com>
2018-12-01 01:21:55 +0900

    no message

--- ha-business/src/main/java/jp/co/ha/business/aop/ApiCommonExecutor.java
+++ ha-business/src/main/java/jp/co/ha/business/aop/ApiCommonExecutor.java
@@ -22,47 +22,6 @@ public class ApiCommonExecutor {
 	/** ロガー */
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
-	//	@Before("execution(* *jp.co.ha.api.service.impl.*ServiceImpl.execute(..)) throws BaseException")
-	//	public void apiRequestLog(JoinPoint jp) {
-	//		for (Object arg : jp.getArgs()) {
-	//			if (arg instanceof BaseApiRequest) {
-	//				// リクエストログを出力
-	//				LOG.infoRes(arg);
-	//			}
-	//		}
-	//	}
-
-	//	@After("execution(* *jp.co.ha.api.service.impl.*ServiceImpl.execute(..)) throws BaseException")
-	//	public void apiResponseSuccessLog(JoinPoint jp) {
-	//		for (Object arg : jp.getArgs()) {
-	//
-	//			System.out.println(jp.getTarget());
-	//			System.out.println(jp.getSignature());
-	//			if (arg instanceof BaseApiResponse) {
-	//				// レスポンスログを出力
-	//				LOG.infoRes(arg);
-	//			}
-	//		}
-	//	}
-
-	//	@AfterThrowing(value = "execution(* *..BaseRestController.appExceptionHandle(..))*", throwing = "e")
-	//	public void apiResponseFailureLog(JoinPoint jp, Exception e) {
-	//		BaseResponse response = null;
-	//		if (e instanceof BaseException) {
-	//			response = new ErrorResponse((BaseException) e);
-	//		} else if (e instanceof InvalidFormatException) {
-	//			InvalidFormatException jfe = (InvalidFormatException) e;
-	//			response = new ErrorResponse(new ApiException(ErrorCode.JSON_FORMAT_ERROR, jfe.getValue() + "はリクエスト形式エラーです"));
-	//		} else if (e instanceof JsonParseException) {
-	//			JsonProcessingException jpe = (JsonProcessingException) e;
-	//			response = new ErrorResponse(new ApiException(ErrorCode.JSON_PARSE_ERROR, jpe.getLocation().getColumnNr() + "行目がjson形式ではありません"));
-	//		} else if (e instanceof JsonProcessingException) {
-	//			JsonProcessingException jpe = (JsonProcessingException) e;
-	//			response = new ErrorResponse(new ApiException(ErrorCode.JSON_PARSE_ERROR, jpe.getLocation().getColumnNr() + ":json形式ではありません"));
-	//		}
-	//		LOG.infoRes(response);
-	//	}
-
 	/**
 	 * APIのリクエスト/レスポンスログ(正常系)を出力する<br>
 	 * レスポンスログ(異常系)は以下のクラスのメソッドで行う<br>
@@ -81,7 +40,7 @@ public class ApiCommonExecutor {
 		// リクエストログを出力
 		Arrays.stream(pjp.getArgs()).filter(e -> e instanceof BaseRequest).forEach(e -> LOG.infoRes(e));
 
-		// jp.co.ha.api.service.impl.*ServiceImpl.execute実行
+		// jp.co.ha.api.controller.*Controller.execute実行
 		BaseResponse response = (BaseResponse) pjp.proceed();
 		// レスポンスログを出力
 		LOG.infoRes(response);
