bef91c75fe63fb28b303e71af42c25c2b23e1c62
kohei-okazaki <kou1210hei@gmail.com>
2018-12-31 13:25:02 +0900

    no message

--- ha-tool/src/main/resources/logback.groovy
+++ ha-tool/src/main/resources/logback.groovy
@@ -6,18 +6,25 @@ import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 import static ch.qos.logback.classic.Level.*
 
 appender("STDOUT", ConsoleAppender) {
+
   encoder(PatternLayoutEncoder) {
     pattern = "%d [%thread] %-5level %logger{10} - %msg%n"
   }
+
 }
+
 appender("FILE", RollingFileAppender) {
+
   file = "logs/main.log"
+
   rollingPolicy(TimeBasedRollingPolicy) {
     fileNamePattern = "main_%d{yyyy-MM-dd}.log"
     maxHistory = 30
   }
+
   encoder(PatternLayoutEncoder) {
     pattern = "%d [%thread] %-5level %logger{10} - %message%n"
   }
+
 }
 root(INFO, ["STDOUT", "FILE"])
