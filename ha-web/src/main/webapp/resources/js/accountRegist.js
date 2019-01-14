713fc57c718fbf12f845b52b351bdf759d704675
kohei-okazaki <kou1210hei@gmail.com>
2019-01-14 21:35:28 +0900

    no message

--- ha-web/src/main/webapp/resources/js/accountRegist.js
+++ ha-web/src/main/webapp/resources/js/accountRegist.js
@@ -4,39 +4,9 @@
  * @returns
  */
 function accountRegist() {
-  // formを作成
   var form = document.createElement('form');
   form.action = '../accountRegist/input.html';
   form.method = 'get';
   document.body.appendChild(form);
   form.submit();
-}
-
-/**
- * 進捗バーの値を変更する関数
- *
- * @returns
- */
-function changeValue() {
-
-  // 入力値を取得
-  var userIdValue = document.getElementById("userId").value;
-  var passwordValue = document.getElementById("password").value;
-  var confirmPasswordValue = document.getElementById("confirmPassword").value;
-
-  // 進捗バーを取得
-  var bar = document.getElementById("bar");
-  var userIdVal = 0;
-  var passwordVal = 0;
-  var confirmPasswordVal = 0;
-  if (userIdValue !== "") {
-    userIdVal = 1;
-  }
-  if (passwordValue !== "") {
-    passwordVal = 1;
-  }
-  if (confirmPasswordValue !== "") {
-    confirmPasswordVal = 1;
-  }
-  bar.value = userIdVal + passwordVal + confirmPasswordVal;
 }
