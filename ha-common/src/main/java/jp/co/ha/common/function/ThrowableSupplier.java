2ccb0b3bc15b88e7919bbea78ccf857f0ca616ed
kohei-okazaki <kou1210hei@gmail.com>
2019-02-04 22:47:35 +0900

    no message

--- ha-common/src/main/java/jp/co/ha/common/function/ThrowableSupplier.java
+++ ha-common/src/main/java/jp/co/ha/common/function/ThrowableSupplier.java
@@ -2,6 +2,13 @@ package jp.co.ha.common.function;
 
 import jp.co.ha.common.exception.BaseException;
 
+/**
+ * {@link java.util.function.Supplier} で例外処理ができないのでthrowできるようにしたクラス<br>
+ * throwできる例外はBaseExceptionを継承したクラスのみとする
+ *
+ * @param <T>
+ *     t型
+ */
 @FunctionalInterface
 public interface ThrowableSupplier<T> {
 
