1fd67130f069480f7ca8007777eb77160713d508
kohei-okazaki <kou1210hei@gmail.com>
2019-03-04 17:27:50 +0900

    no message

--- ha-business/src/main/java/jp/co/ha/business/db/crud/create/impl/HealthInfoCreateServiceImpl.java
+++ ha-business/src/main/java/jp/co/ha/business/db/crud/create/impl/HealthInfoCreateServiceImpl.java
@@ -1,8 +1,9 @@
 package jp.co.ha.business.db.crud.create.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.ha.business.db.SqlSessionFactory;
 import jp.co.ha.business.db.crud.create.HealthInfoCreateService;
 import jp.co.ha.common.db.annotation.Insert;
 import jp.co.ha.common.exception.BaseException;
@@ -15,27 +16,27 @@ import jp.co.ha.db.mapper.HealthInfoMapper;
  */
 public class HealthInfoCreateServiceImpl implements HealthInfoCreateService {
 
-	@Autowired
-	private HealthInfoMapper mapper;
+//	@Autowired
+//	private HealthInfoMapper mapper;
 
-	@Insert
-	@Override
-	@Transactional
-	public void create(HealthInfo entity) throws BaseException {
-		mapper.insert(entity);
-	}
-
-//	/**
-//	 * {@inheritDoc}
-//	 */
 //	@Insert
 //	@Override
 //	@Transactional
 //	public void create(HealthInfo entity) throws BaseException {
-//		try (SqlSession session = SqlSessionFactory.getInstance().getSqlSession()) {
-//			HealthInfoMapper mapper = session.getMapper(HealthInfoMapper.class);
-//			mapper.insert(entity);
-//			session.commit();
-//		}
+//		mapper.insert(entity);
 //	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Insert
+	@Override
+	@Transactional
+	public void create(HealthInfo entity) throws BaseException {
+		try (SqlSession session = SqlSessionFactory.getInstance().getSqlSession()) {
+			HealthInfoMapper mapper = session.getMapper(HealthInfoMapper.class);
+			mapper.insert(entity);
+			session.commit();
+		}
+	}
 }
