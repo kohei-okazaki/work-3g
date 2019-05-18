607069352bc79353201c54ab2f69a979ef8c4720
kou1210hei <kou1210hei@gmail.com>
2019-05-18 13:48:25 +0900

    no message

--- ha-dashboard/src/main/webapp/resources/js/common/apiReferenceGeneralModal.js
+++ ha-dashboard/src/main/webapp/resources/js/common/apiReferenceGeneralModal.js
@@ -1,59 +1,70 @@
 $(function() {
 
-  // モーダルウィンドウを出現させるクリックイベント
-  $("#api-reference-general-modal-open").click(function() {
-
-    // キーボード操作などにより、オーバーレイが多重起動するのを防止する
-    $(this).blur();  // ボタンからフォーカスを外す
-    if ($("#api-reference-general-modal-overlay")[0]) {
-      return false;
-    }
-    // if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;
-    // //現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-
-    // オーバーレイを出現させる
-    $("body").append('<div id="api-reference-general-modal-overlay"></div>');
-    $("#api-reference-general-modal-overlay").fadeIn("slow");
-
-    // コンテンツをセンタリングする
-    centeringModalSyncer();
-
-    // コンテンツをフェードインする
-    $("#api-reference-general-modal-content").fadeIn("slow");
-
-    // [#modal-overlay]、または[#modal-close]をクリックしたら…
-    $("#api-reference-general-modal-overlay,#api-reference-general-modal-close").unbind().click(function() {
-
-      // [#modal-content]と[#modal-overlay]をフェードアウトした後に…
-      $("#api-reference-general-modal-content,#api-reference-general-modal-overlay").fadeOut("slow", function() {
-
-        // [#modal-overlay]を削除する
-        $('#api-reference-general-modal-overlay').remove();
-
-      });
-
-    });
-
+  $("#api-reference-general-modal").css({
+    "cursor" : "pointer"
   });
 
-  // リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-  $(window).resize(centeringModalSyncer);
+  // modalの内容を非表示
+  $("#api-reference-general-modal-content").css({
+    "display" : "none"
+  });
 
-  // センタリングを実行する関数
-  function centeringModalSyncer() {
+  // modalを開くイベント処理
+  $("#api-reference-general-modal").on("click", function() {
+    $("body").append('<div id="api-reference-general-modal-bg"></div>');
+    $("#api-reference-general-modal-bg").css({
+      "display" : "none",
+      "width" : "100%",
+      "height" : "100%",
+      "background-color" : "rgba(0,0,0,0.5)",
+      "position" : "fixed",
+      "top" : "0",
+      "left" : "0",
+      "z-index" : "1"
+    });
 
-    // 画面(ウィンドウ)の幅、高さを取得
-    var w = $(window).width();
-    var h = $(window).height();
+    $("body").append('<div id="api-reference-general-modal-content"><div class="contentLayout"><b>API 健康情報照会</b><br><br><div class="contentLayout"><label>jsonにより指定されたデータから、ユーザの登録された健康情報の照会を行う</label><br><label>リクエストURL:/ha-api/healthInfoReference</label></div></div></div>');
+    $("#api-reference-general-modal-content").css({
+      "display" : "none",
+      "width" : "auto",
+      "height" : "auto",
+      "margin" : "0",
+      "padding" : "0",
+      "background-color" : "#ffffff",
+      "color" : "#666666",
+      "position" : "fixed",
+      "z-index" : "9999"
+    });
 
-    // コンテンツ(#modal-content)の幅、高さを取得
-    // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-    var cw = $("#api-reference-general-modal-content").outerWidth();
-    var ch = $("#api-reference-general-modal-content").outerHeight();
+    // modalの位置を画面中央に計算する関数を実行
+    modalResize();
 
-    // センタリングを実行する
-    $("#api-reference-general-modal-content").css({"left": ((w - cw) / 2) + "px","top": ((h - ch) / 2) + "px"});
+    // modalウィンドウを表示
+    $("#api-reference-general-modal-bg,#api-reference-general-modal-content").fadeIn("slow");
 
-  }
+    // modal背景のどこかをクリックしたらモーダルを閉じる
+    $("#api-reference-general-modal-bg").on("click", function() {
+      $("#api-reference-general-modal-content,#api-reference-general-modal-bg").fadeOut("slow", function() {
+        // modalを削除
+        $('#api-reference-general-modal-bg').remove();
+        $('#api-reference-general-modal-content').remove();
+      });
+    });
 
+    // 画面の左上からmodalの横幅・高さを引き、その値を2で割り画面中央に表示
+    $(window).resize(modalResize);
+
+    function modalResize() {
+      var w = $(window).width();
+      var h = $(window).height();
+      var cw = $("#api-reference-general-modal-content").outerWidth();
+      var ch = $("#api-reference-general-modal-content").outerHeight();
+      //取得した値をcssに追加する
+      $("#api-reference-general-modal-content").css({
+        "left" : ((w - cw) / 2) + "px",
+        "top" : ((h - ch) / 2) + "px"
+      });
+    }
+
+  });
 });
