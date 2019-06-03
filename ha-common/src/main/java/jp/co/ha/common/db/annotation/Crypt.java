0242fb962971e99cc9ad6461282408417faf0c85
kou1210hei <kou1210hei@gmail.com>
2019-06-03 21:25:04 +0900

    no message

---
+++ ha-common/src/main/java/jp/co/ha/common/db/annotation/Crypt.java
@@ -0,0 +1,20 @@
+package jp.co.ha.common.db.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 暗号化カラムに付与するマーカーアノテーション
+ *
+ */
+@Inherited
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Crypt {
+
+}
