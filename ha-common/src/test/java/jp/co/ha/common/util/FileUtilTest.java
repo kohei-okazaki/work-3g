87d9ab8cbcecbcd1a52510c6327fa4e15297bf0b
kou1210hei <kou1210hei@gmail.com>
2019-06-18 23:30:13 +0900

    no message

--- ha-common/src/test/java/jp/co/ha/common/util/FileUtilTest.java
+++ ha-common/src/test/java/jp/co/ha/common/util/FileUtilTest.java
@@ -16,8 +16,8 @@ public class FileUtilTest {
 	@Test
 	public void toZipTest() {
 
-		String destPath = "C:\\app\\pleiades\\workspace\\java-11\\file\\result\\sample.zip";
-		List<File> list = FileUtil.getFileList("C:\\app\\pleiades\\workspace\\java-11\\file\\zip");
+		String destPath = "C:\\app\\git\\work-3g\\ha-common\\src\\test\\resources\\zip\\dest\\result.zip";
+		List<File> list = FileUtil.getFileList("C:\\app\\git\\work-3g\\ha-common\\src\\test\\resources\\zip\\src");
 		File zip = FileUtil.compressZip(list, destPath);
 		assertTrue(zip.isFile());
 	}
