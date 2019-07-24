771d0b66695bfae3afa7177dd3817c49bfab67b6
kou1210hei <kou1210hei@gmail.com>
2019-07-24 14:59:00 +0900

    no message

--- ha-build/bat/maven-build.bat
+++ ha-build/bat/maven-build.bat
@@ -17,30 +17,30 @@ echo jar.version = %ver%
 
 rem commonのjarを作成
 cd %BASE_DIR%\ha-common
-call mvn clean package -DskipTests
+rem call mvn clean package -DskipTests
 call mvn install
-call mvn install:install-file -Dfile=target\ha-common-%ver%.jar -DgroupId=jp.co.ha -DartifactId=common -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn install:install-file -Dfile=target\common-%ver%.jar -DgroupId=jp.co.ha -DartifactId=common -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 rem dbのjarを作成
 cd %BASE_DIR%\ha-db
-call mvn clean package -DskipTests
+rem call mvn clean package -DskipTests
 call mvn install
-call mvn install:install-file -Dfile=target\ha-db-%ver%.jar -DgroupId=jp.co.ha -DartifactId=db -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn install:install-file -Dfile=target\db-%ver%.jar -DgroupId=jp.co.ha -DartifactId=db -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 rem webのjarを作成
 cd %BASE_DIR%\ha-web
-call mvn clean package -DskipTests
+rem call mvn clean package -DskipTests
 call mvn install
-call mvn install:install-file -Dfile=target\ha-web-%ver%.jar -DgroupId=jp.co.ha -DartifactId=web -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn install:install-file -Dfile=target\web-%ver%.jar -DgroupId=jp.co.ha -DartifactId=web -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 rem businessのjarを作成
 cd %BASE_DIR%\ha-business
-call mvn clean package -DskipTests
+rem call mvn clean package -DskipTests
 call mvn install
-call mvn install:install-file -Dfile=target\ha-business-%ver%.jar -DgroupId=jp.co.ha -DartifactId=business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn install:install-file -Dfile=target\business-%ver%.jar -DgroupId=jp.co.ha -DartifactId=business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 cd %~dp0
