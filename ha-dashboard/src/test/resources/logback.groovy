e1190a0de451e631878440cb2670ef323fac8a27
kou1210hei <kou1210hei@gmail.com>
2019-10-19 14:30:32 +0900

    no message

--- ha-dashboard/src/test/resources/logback.groovy
+++ ha-dashboard/src/test/resources/logback.groovy
@@ -9,7 +9,7 @@ import static ch.qos.logback.classic.Level.*
 
 scan("30 seconds")
 
-def FILE_PATH = "D:/app/testlogs";
+def FILE_PATH = "C:/app/testlogs";
 def ENCODE = "UTF-8";
 
 appender("STDOUT", ConsoleAppender) {
