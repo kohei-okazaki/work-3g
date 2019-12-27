1598422349eb1c2d53988391fe7a45d0e36a8028
kou1210hei <kou1210hei@gmail.com>
2019-12-27 20:46:20 +0900

    no message

---
+++ ha-api/src/test/java/jp/co/ha/api/BaseApiTest.java
@@ -0,0 +1,45 @@
+package jp.co.ha.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import jp.co.ha.common.log.Logger;
+import jp.co.ha.common.log.LoggerFactory;
+import jp.co.ha.common.system.BeanLoader;
+
+/**
+ * API基底クラス
+ *
+ * @since 1.0
+ */
+@ContextConfiguration(locations = { "classpath:common-context.xml", "classpath:db-context.xml",
+		"classpath:web-context.xml", "classpath:business-context.xml", "classpath:api-context.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BaseApiTest {
+
+	/** LOG */
+	protected static final Logger LOG = LoggerFactory.getLogger(BaseApiTest.class);
+
+	/**
+	 * before
+	 */
+	@Before
+	public void before() {
+		LOG.debug("BaseBusinessTest#before");
+		BeanLoader.setContext(new ClassPathXmlApplicationContext(
+				new String[] { "classpath:common-context.xml", "classpath:db-context.xml", "classpath:web-context.xml",
+						"classpath:business-context.xml", "classpath:api-context.xml" }));
+	}
+
+	/**
+	 * after
+	 */
+	@After
+	public void after() {
+		LOG.debug("BaseBusinessTest#after");
+	}
+}
---
+++ ha-api/src/test/java/jp/co/ha/api/healthinfo/HealthInfoRefenceApiTest.java
@@ -0,0 +1,62 @@
+package jp.co.ha.api.healthinfo;
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import jp.co.ha.api.BaseApiTest;
+import jp.co.ha.business.api.response.HealthInfoReferenceResponse;
+import jp.co.ha.common.system.SystemConfig;
+
+/**
+ * 健康情報照会APIテストクラス
+ *
+ * @since 1.0
+ */
+public class HealthInfoRefenceApiTest extends BaseApiTest {
+
+	/** API呼び出しクラス */
+	@Autowired
+	private RestTemplate client;
+	/** システム設定情報 */
+	@Autowired
+	private SystemConfig systemConfig;
+
+	/**
+	 * 照会APIのテストメソッド
+	 */
+	@Test
+	public void testReference() {
+		try {
+			URI uri = new URI(this.systemConfig.getBaseApiUrl() + "/healthInfoReference");
+
+			// ヘッダ作成
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+
+			// src/test/resources配下のJSONファイルインスタンスを引数に渡すように、JSOM文字列を使わないようにする
+			String json = "{\"requestType\":\"002\",\"userId\":\"master\",\"healthInfoId\":10,\"apiKey\":\"1bc073967f4c24f000d0fc34006cf8de83752a9d8fde68278b18c033bbb68d2c\"}";
+
+			RequestEntity<?> requestEntity = new RequestEntity<>(json, headers, HttpMethod.POST, uri);
+
+			ResponseEntity<HealthInfoReferenceResponse> responseEntity = this.client.exchange(requestEntity,
+					HealthInfoReferenceResponse.class);
+
+			LOG.info(responseEntity.getStatusCode().toString());
+
+			HealthInfoReferenceResponse response = responseEntity.getBody();
+			LOG.infoRes(response);
+
+		} catch (Exception e) {
+			LOG.error("APIの実行に失敗しました", e);
+		}
+	}
+
+}
