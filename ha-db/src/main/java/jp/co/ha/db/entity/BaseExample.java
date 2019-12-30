131febb5531cb546edd767cd66a768f52ea4daaa
kohei.okazaki <kou1210hei@gmail.com>
2019-12-30 17:59:23 +0900

    no message

--- ha-db/src/main/java/jp/co/ha/db/entity/BaseExample.java
+++ ha-db/src/main/java/jp/co/ha/db/entity/BaseExample.java
@@ -2,7 +2,9 @@ package jp.co.ha.db.entity;
 
 /**
  * MyBatisのExampleクラスの基底Exampleクラス<br>
- * 検索上限数等すべてのTableに対し、共通で処理すべきものを定義する
+ * 検索上限数等すべてのTableに対し、共通で処理すべきものを定義する<br>
+ * TODO ロック処理はまだどのMapper.xmlに反映させていない<br>
+ * TODO ここで定義したオプションを使う場合、各Mapper.xmlにその記述を入れること<br>
  *
  * @since 1.0
  */
@@ -10,6 +12,8 @@ public abstract class BaseExample {
 
 	/** 検索上限数 */
 	protected Integer limit;
+	/** ロック有無 */
+	protected Boolean lock;
 
 	/**
 	 * limitを返す
@@ -24,8 +28,29 @@ public abstract class BaseExample {
 	 * limitを設定する
 	 *
 	 * @param limit
+	 *     検索上限数
 	 */
 	public void setLimit(Integer limit) {
 		this.limit = limit;
 	}
+
+	/**
+	 * lockを返す
+	 *
+	 * @return lock
+	 */
+	public Boolean getLock() {
+		return lock;
+	}
+
+	/**
+	 * lockを設定する
+	 *
+	 * @param lock
+	 *     ロック有無
+	 */
+	public void setLock(Boolean lock) {
+		this.lock = lock;
+	}
+
 }
