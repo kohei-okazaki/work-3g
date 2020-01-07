a36f06edad8b772611178a4bde86b7220b89222b
kohei.okazaki <kou1210hei@gmail.com>
2020-01-08 00:47:48 +0900

    no message

--- ha-common/src/main/java/jp/co/ha/common/util/StringUtil.java
+++ ha-common/src/main/java/jp/co/ha/common/util/StringUtil.java
@@ -38,6 +38,24 @@ public class StringUtil {
 	public static final String THRASH = "/";
 	/** アンダースコア */
 	public static final String UNDER_SCORE = "_";
+	/** 半角ｶﾅ */
+	private static final char[] HALF_KANA = { '｡', '｢', '｣', '､', '･',
+			'ｦ', 'ｧ', 'ｨ', 'ｩ', 'ｪ', 'ｫ', 'ｬ', 'ｭ', 'ｮ', 'ｯ', 'ｰ', 'ｱ', 'ｲ',
+			'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｸ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ',
+			'ﾀ', 'ﾁ', 'ﾂ', 'ﾃ', 'ﾄ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ', 'ﾉ', 'ﾊ', 'ﾋ', 'ﾌ',
+			'ﾍ', 'ﾎ', 'ﾏ', 'ﾐ', 'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾖ', 'ﾗ', 'ﾘ', 'ﾙ',
+			'ﾚ', 'ﾛ', 'ﾜ', 'ﾝ', 'ﾞ', 'ﾟ' };
+	/** 全角カナ */
+	private static final char[] FULLKANA = { '。', '「', '」', '、', '・',
+			'ヲ', 'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ャ', 'ュ', 'ョ', 'ッ', 'ー', 'ア', 'イ',
+			'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ',
+			'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ',
+			'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル',
+			'レ', 'ロ', 'ワ', 'ン', '゛', '゜' };
+	/** 半角ｶﾅの1文字目 */
+	private static final char HALF_KANA_FIRST_CHAR = HALF_KANA[0];
+	/** 半角ｶﾅの最後の1文字目 */
+	private static final char HALF_KANA_LAST_CHAR = HALF_KANA[HALF_KANA.length - 1];
 
 	/**
 	 * プライベートコンストラクタ
@@ -96,9 +114,7 @@ public class StringUtil {
 	/**
 	 * 半角スペースでPaddingする
 	 *
-	 * @see StringUtil#padding(String target, int length, String str,
-	 * PaddingType paddingType)
-	 *
+	 * @see StringUtil#padding
 	 * @param target
 	 *     対象文字列
 	 * @param length
@@ -242,6 +258,137 @@ public class StringUtil {
 			return value;
 		}
 		return value.replaceAll("(\\s|　)", "");
+	}
+
+	/**
+	 * 文字列中の半角ｶﾅを全角カナに変換した文字列を返す
+	 *
+	 * @param s
+	 *     変換前文字列
+	 * @return 変換後文字列
+	 */
+	public static String toFullKana(String s) {
+		if (s == null || s.length() == 0) {
+			return s;
+		} else if (s.length() == 1) {
+			return toFullKana(s.charAt(0)) + "";
+		} else {
+			StringBuffer sb = new StringBuffer(s);
+			int i = 0;
+			for (i = 0; i < sb.length() - 1; i++) {
+				char originalChar1 = sb.charAt(i);
+				char originalChar2 = sb.charAt(i + 1);
+				char margedChar = mergeChar(originalChar1, originalChar2);
+				if (margedChar != originalChar1) {
+					sb.setCharAt(i, margedChar);
+					sb.deleteCharAt(i + 1);
+				} else {
+					char convertedChar = toFullKana(originalChar1);
+					if (convertedChar != originalChar1) {
+						sb.setCharAt(i, convertedChar);
+					}
+				}
+			}
+			if (i < sb.length()) {
+				char originalChar1 = sb.charAt(i);
+				char convertedChar = toFullKana(originalChar1);
+				if (convertedChar != originalChar1) {
+					sb.setCharAt(i, convertedChar);
+				}
+			}
+			return sb.toString();
+		}
+
+	}
+
+	/**
+	 * 半角カタカナから全角カタカナへ変換します。
+	 *
+	 * @param c
+	 *     変換前の文字
+	 * @return 変換後の文字
+	 */
+	private static char toFullKana(char c) {
+		if (c >= HALF_KANA_FIRST_CHAR && c <= HALF_KANA_LAST_CHAR) {
+			return FULLKANA[c - HALF_KANA_FIRST_CHAR];
+		} else {
+			return c;
+		}
+	}
+
+	/**
+	 * 2文字目が濁点・半濁点で、1文字目に加えることができる場合は、合成した文字を返します。 <br>
+	 * 合成ができないときは、c1を返します。
+	 *
+	 * @param c1
+	 *     変換前の1文字目
+	 * @param c2
+	 *     変換前の2文字目
+	 * @return 変換後の文字
+	 */
+	private static char mergeChar(char c1, char c2) {
+		if (c2 == 'ﾞ') {
+			if ("ｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾊﾋﾌﾍﾎ".indexOf(c1) >= 0) {
+				switch (c1) {
+				case 'ｶ':
+					return 'ガ';
+				case 'ｷ':
+					return 'ギ';
+				case 'ｸ':
+					return 'グ';
+				case 'ｹ':
+					return 'ゲ';
+				case 'ｺ':
+					return 'ゴ';
+				case 'ｻ':
+					return 'ザ';
+				case 'ｼ':
+					return 'ジ';
+				case 'ｽ':
+					return 'ズ';
+				case 'ｾ':
+					return 'ゼ';
+				case 'ｿ':
+					return 'ゾ';
+				case 'ﾀ':
+					return 'ダ';
+				case 'ﾁ':
+					return 'ヂ';
+				case 'ﾂ':
+					return 'ヅ';
+				case 'ﾃ':
+					return 'デ';
+				case 'ﾄ':
+					return 'ド';
+				case 'ﾊ':
+					return 'バ';
+				case 'ﾋ':
+					return 'ビ';
+				case 'ﾌ':
+					return 'ブ';
+				case 'ﾍ':
+					return 'ベ';
+				case 'ﾎ':
+					return 'ボ';
+				}
+			}
+		} else if (c2 == 'ﾟ') {
+			if ("ﾊﾋﾌﾍﾎ".indexOf(c1) >= 0) {
+				switch (c1) {
+				case 'ﾊ':
+					return 'パ';
+				case 'ﾋ':
+					return 'ピ';
+				case 'ﾌ':
+					return 'プ';
+				case 'ﾍ':
+					return 'ペ';
+				case 'ﾎ':
+					return 'ポ';
+				}
+			}
+		}
+		return c1;
 	}
 
 	/**
