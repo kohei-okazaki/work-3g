9d171cbbffecd7fe53f488f6acf5c2b7bab16309
kohei.okazaki <kou1210hei@gmail.com>
2020-02-22 11:55:30 +0900

    no message

--- ha-build/shell/aws-app-server-build.sh
+++ ha-build/shell/aws-app-server-build.sh
@@ -13,27 +13,25 @@
 ########################################
 # ログディレクトリを定義
 LOG_DIR=/var/log
-APP_LOG_DIR=/var/log/app
+APP_LOG_DIR=$LOG_DIR/app
 # ログファイルを定義
 DASHBOARD_LOG=dashboard.log
 
-# Rootユーザに変更
-sudo su - root
 # /var/logへ移動
-cd ${LOG_DIR}
+cd $LOG_DIR
 # appディレクトリ作成
-mkdir -m 777 app
+sudo mkdir -m 777 app
 # appディレクトリへ移動
-cd ${APP_LOG_DIR}
+cd $APP_LOG_DIR
 # 空ファイルを作成
-touch ${DASHBOARD_LOG}
+touch $DASHBOARD_LOG
 # ファイルのパーミッションを変更
-chmod 777 ${DASHBOARD_LOG}
+chmod 777 $DASHBOARD_LOG
 
 ########################################
 # 2:システム日時を設定
 ########################################
-# ユーザをec2-userに変更
-sudo su - ec2-user
 # /home/ec2-user/clock_newファイルを/etc/sysconfig/配下にコピーする
-sudo -u root cp -i /home/ec2-user/clock_new /etc/sysconfig/
+sudo cp -i /home/ec2-user/clock /etc/sysconfig/
+# 時間帯ファイルにシンボリックリンク
+sudo ln -sf /usr/share/zoneinfo/Japan /etc/localtime
