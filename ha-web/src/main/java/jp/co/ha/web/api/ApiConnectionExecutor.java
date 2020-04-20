8e7ae2b5fad73a5513317019d211456620cb1381
kohei.okazaki <kou1210hei@gmail.com>
2020-04-21 01:10:27 +0900

    no message

--- ha-web/src/main/java/jp/co/ha/web/api/ApiConnectionExecutor.java
+++ ha-web/src/main/java/jp/co/ha/web/api/ApiConnectionExecutor.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 
 import jp.co.ha.common.log.Logger;
 import jp.co.ha.common.log.LoggerFactory;
-import jp.co.ha.web.controller.BaseJsonController;
 import jp.co.ha.web.form.BaseApiRequest;
 import jp.co.ha.web.form.BaseApiResponse;
 
@@ -29,10 +28,7 @@ public class ApiConnectionExecutor {
     /**
      * APIのリクエスト/レスポンスログ(正常系)を出力する<br>
      * レスポンスログ(異常系)は以下のクラスのメソッドで行う<br>
-     * <ul>
-     * <li>{@link BaseJsonController#jsonExceptionHandle}</li>
-     * <li>{@link BaseJsonController#appExceptionHandle}</li>
-     * </ul>
+     * {@linkplain jp.co.ha.api.config.RestApiExceptionHandler#handleException}
      *
      * @param pjp
      *     ProceedingJoinPoint
