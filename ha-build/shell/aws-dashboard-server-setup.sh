be10f77a9e713630d47be80100ba565fcb52440b
kohei.okazaki <kou1210hei@gmail.com>
2020-04-25 21:03:35 +0900

    no message

--- ha-build/shell/aws-dashboard-server-setup.sh
+++ ha-build/shell/aws-dashboard-server-setup.sh
@@ -9,6 +9,9 @@
 LOG_DIR=/var/log
 APP_LOG_DIR=$LOG_DIR/app
 DASHBOARD_LOG=dashboard.log
+APP_BASE_DIR=/home/admin
+APP_DATA_DIR=$APP_BASE_DIR/data
+HEALTH_INFO_REF_DIR=$APP_DATA_DIR/healthInfoReference
 
 ##########################################################################
 # 1 log setup
@@ -24,3 +27,16 @@ chmod 777 $DASHBOARD_LOG
 ##########################################################################
 sudo cp -i /home/ec2-user/clock /etc/sysconfig/
 sudo ln -sf /usr/share/zoneinfo/Japan /etc/localtime
+
+##########################################################################
+# 3 app base dir create
+##########################################################################
+# create /home/admin
+cd /home
+sudo mkdir -m 777 $APP_BASE_DIR
+# create /home/admin/data
+cd $APP_BASE_DIR
+sudo mkdir -m 777 $APP_DATA_DIR
+# create /home/admin/data/healthInfoReference
+cd $APP_DATA_DIR
+sudo mkdir -m 777 $HEALTH_INFO_REF_DIR
