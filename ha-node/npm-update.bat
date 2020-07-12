9b81492e3cdbfcf1129b6e0f6f0f91386febe0f4
kohei.okazaki <kou1210hei@gmail.com>
2020-07-12 15:09:30 +0900

    no message

---
+++ ha-node/npm-update.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem ------------------------------------------------------------------------
+rem
+rem Nodeのモジュールの更新を行う
+rem package.jsonのdependencies内のversionの最新化
+rem
+rem ------------------------------------------------------------------------
+
+cls
+
+echo start npm update
+
+npm update
+
---
+++ ha-node/routes/business/healthinfo.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var util = require('../util/util');
+var env = require('../conf/env');
+
+/**
+ * 健康情報計算の処理<br>
+ * 身長と体重からBMIと標準体重を計算する
+ * 
+ * @param req
+ *            リクエスト情報
+ * @param res
+ *            レスポンス情報
+ * @param next
+ */
+router.post('/', function(req, res, next) {
+
+    console.log("環境名=" + env.config.name);
+    console.log(req.body);
+
+    let meter_height = req.body['height'] / 100;
+    let bmi = util.calc_bmi(req.body['weight'], meter_height);
+    bmi = util.round(bmi, 3);
+
+    let standard_weight = util.calc_standard_weight(meter_height);
+    standard_weight = util.round(standard_weight, 3);
+
+    let basic_health_info = {
+        'height' : req.body['height'],
+        'weight' : req.body['weight'],
+        'bmi' : bmi,
+        'standard_weight' : standard_weight
+    }
+    let res_body = {
+        'result' : 0,
+        'basic_health_info' : basic_health_info
+    };
+    console.log(res_body);
+    res.json(res_body);
+})
+
+module.exports = router;
