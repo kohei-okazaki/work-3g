0f6de05c9cc49a3af33a6ced482feeb034e7deca
kohei.okazaki <kou1210hei@gmail.com>
2020-09-05 09:54:28 +0900

    mavenビルドのスクリプトを動くように修正

--- ha-build/bat/maven-build.bat
+++ ha-build/bat/maven-build.bat
@@ -17,35 +17,39 @@ echo jar.version = %ver%
 
 rem commonのjarを作成
 cd %BASE_DIR%\ha-common
-rem call mvn clean package -DskipTests
-call mvn clean package -Dmaven.test.skip=true
-rem call mvn install
-rem call mvn install:install-file -Dfile=target\common-%ver%.jar -DgroupId=jp.co.ha -DartifactId=common -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn clean package -Dmaven.test.skip=true -Pec2
+call mvn install:install-file -Dfile=target\common-%ver%.jar -DgroupId=jp.co.ha -DartifactId=common -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 rem dbのjarを作成
 cd %BASE_DIR%\ha-db
-rem call mvn clean package -DskipTests
-call mvn clean package -Dmaven.test.skip=true
-rem call mvn install
-rem call mvn install:install-file -Dfile=target\db-%ver%.jar -DgroupId=jp.co.ha -DartifactId=db -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn clean package -Dmaven.test.skip=true -Pec2
+call mvn install:install-file -Dfile=target\db-%ver%.jar -DgroupId=jp.co.ha -DartifactId=db -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 rem webのjarを作成
 cd %BASE_DIR%\ha-web
-rem call mvn clean package -DskipTests
-call mvn clean package -Dmaven.test.skip=true
-rem call mvn install
-rem call mvn install:install-file -Dfile=target\web-%ver%.jar -DgroupId=jp.co.ha -DartifactId=web -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+call mvn clean package -Dmaven.test.skip=true -Pec2
+call mvn install:install-file -Dfile=target\web-%ver%.jar -DgroupId=jp.co.ha -DartifactId=web -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
 
 rem businessのjarを作成
 cd %BASE_DIR%\ha-business
-rem call mvn clean package -DskipTests
-call mvn clean package -Dmaven.test.skip=true
-rem call mvn install
+call mvn clean package -Dmaven.test.skip=true -Pec2
+call mvn install:install-file -Dfile=target\business-%ver%.jar -DgroupId=jp.co.ha -DartifactId=business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+
+
+rem apiのwarを作成
+cd %BASE_DIR%\ha-api
+call mvn clean package -Dmaven.test.skip=true -Pec2
 rem call mvn install:install-file -Dfile=target\business-%ver%.jar -DgroupId=jp.co.ha -DartifactId=business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
 
-call deploy.bat
+
+rem dashboardのwarを作成
+cd %BASE_DIR%\ha-dashboard
+call mvn clean package -Dmaven.test.skip=true -Pec2
+rem call mvn install:install-file -Dfile=target\business-%ver%.jar -DgroupId=jp.co.ha -DartifactId=business -Dversion=%ver% -Dpackaging=jar -DgeneratePom=true -Dmaven.test.skip
+
+rem call deploy.bat
 
 cd %~dp0
