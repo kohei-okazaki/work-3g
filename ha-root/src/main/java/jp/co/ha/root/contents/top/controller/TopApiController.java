c5edfa5415c595d8e96646c9f6a787057e7408ec
kohei-okazaki <kou1210hei@gmail.com>
2021-02-27 17:48:13 +0900

    no message

--- ha-root/src/main/java/jp/co/ha/root/contents/top/controller/TopApiController.java
+++ ha-root/src/main/java/jp/co/ha/root/contents/top/controller/TopApiController.java
@@ -12,12 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import jp.co.ha.business.db.crud.read.AccountSearchService;
 import jp.co.ha.business.db.crud.read.HealthInfoSearchService;
 import jp.co.ha.common.util.BeanUtil;
 import jp.co.ha.common.util.DateTimeUtil;
+import jp.co.ha.common.util.DateTimeUtil.DateFormatType;
+import jp.co.ha.common.util.StringUtil;
 import jp.co.ha.root.base.BaseRootApiController;
 import jp.co.ha.root.contents.top.request.TopApiRequest;
 import jp.co.ha.root.contents.top.response.TopApiResponse;
@@ -32,6 +33,9 @@ import jp.co.ha.root.type.RootApiResult;
 public class TopApiController
         extends BaseRootApiController<TopApiRequest, TopApiResponse> {
 
+    /** ObjectMapper */
+    @Autowired
+    private ObjectMapper mapper;
     /** アカウント情報検索サービス */
     @Autowired
     private AccountSearchService accountSearchService;
@@ -49,16 +53,9 @@ public class TopApiController
     @GetMapping(value = "top", produces = { MediaType.APPLICATION_JSON_VALUE })
     public TopApiResponse top(@RequestParam Map<String, String> request) {
 
-        TopApiRequest apiRequest = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .convertValue(request, TopApiRequest.class);
-
-        LocalDate targetDate = DateTimeUtil.toLocalDate(DateTimeUtil.getSysDate());
-        LocalDateTime from = LocalDateTime.of(targetDate.getYear(),
-                targetDate.getMonthValue(), 1, 0, 0, 0);
-        LocalDateTime to = LocalDateTime.of(targetDate.getYear(),
-                targetDate.getMonthValue(),
-                DateTimeUtil.getLastDayOfMonth(targetDate), 23, 59, 59);
+        TopApiRequest apiRequest = mapper.convertValue(request, TopApiRequest.class);
+        LocalDateTime from = getFrom(apiRequest);
+        LocalDateTime to = getTo(apiRequest);
 
         TopApiResponse response = new TopApiResponse();
         response.setRootApiResult(RootApiResult.SUCCESS);
@@ -90,12 +87,9 @@ public class TopApiController
     @GetMapping(value = "top/healthinfo", produces = { MediaType.APPLICATION_JSON_VALUE })
     public TopApiResponse healthinfo(@RequestParam Map<String, String> request) {
 
-        LocalDate targetDate = DateTimeUtil.toLocalDate(DateTimeUtil.getSysDate());
-        LocalDateTime from = LocalDateTime.of(targetDate.getYear(),
-                targetDate.getMonthValue(), 1, 0, 0, 0);
-        LocalDateTime to = LocalDateTime.of(targetDate.getYear(),
-                targetDate.getMonthValue(),
-                DateTimeUtil.getLastDayOfMonth(targetDate), 23, 59, 59);
+        TopApiRequest apiRequest = mapper.convertValue(request, TopApiRequest.class);
+        LocalDateTime from = getFrom(apiRequest);
+        LocalDateTime to = getTo(apiRequest);
 
         TopApiResponse response = new TopApiResponse();
         response.setRootApiResult(RootApiResult.SUCCESS);
@@ -120,12 +114,9 @@ public class TopApiController
     @GetMapping(value = "top/account", produces = { MediaType.APPLICATION_JSON_VALUE })
     public TopApiResponse account(@RequestParam Map<String, String> request) {
 
-        LocalDate targetDate = DateTimeUtil.toLocalDate(DateTimeUtil.getSysDate());
-        LocalDateTime from = LocalDateTime.of(targetDate.getYear(),
-                targetDate.getMonthValue(), 1, 0, 0, 0);
-        LocalDateTime to = LocalDateTime.of(targetDate.getYear(),
-                targetDate.getMonthValue(),
-                DateTimeUtil.getLastDayOfMonth(targetDate), 23, 59, 59);
+        TopApiRequest apiRequest = mapper.convertValue(request, TopApiRequest.class);
+        LocalDateTime from = getFrom(apiRequest);
+        LocalDateTime to = getTo(apiRequest);
 
         TopApiResponse response = new TopApiResponse();
         response.setRootApiResult(RootApiResult.SUCCESS);
@@ -138,5 +129,44 @@ public class TopApiController
                 }).collect(Collectors.toList()));
 
         return response;
+    }
+
+    /**
+     * 処理対象年月(開始)を返す
+     *
+     * @param request
+     *     Top情報取得APIリクエスト
+     * @return 処理対象年月(開始)
+     */
+    private LocalDateTime getFrom(TopApiRequest request) {
+        LocalDate date = getDate(request);
+        return LocalDateTime.of(date.getYear(), date.getMonthValue(), 1, 0, 0, 0);
+    }
+
+    /**
+     * 処理対象年月(終了)を返す
+     *
+     * @param request
+     *     Top情報取得APIリクエスト
+     * @return 処理対象年月(終了)
+     */
+    private LocalDateTime getTo(TopApiRequest request) {
+        LocalDate date = getDate(request);
+        return LocalDateTime.of(date.getYear(), date.getMonthValue(),
+                DateTimeUtil.getLastDayOfMonth(date), 23, 59, 59);
+    }
+
+    /**
+     * 対象年月を返す
+     *
+     * @param request
+     *     Top情報取得APIリクエスト
+     * @return 対象年月
+     */
+    private LocalDate getDate(TopApiRequest request) {
+        return StringUtil.isEmpty(request.getDate())
+                ? DateTimeUtil.toLocalDate(DateTimeUtil.getSysDate())
+                : DateTimeUtil.toLocalDate(request.getDate() + "01",
+                        DateFormatType.YYYYMMDD_NOSEP_STRICT);
     }
 }
