8b33f683ba071aac44479414fceab836dbef564e
kohei-okazaki <kou1210hei@gmail.com>
2021-03-06 23:56:39 +0900

    検索と更新SQLの自動生成vueを作成

--- ha-asset/04_docs/pages/db/select.vue
+++ ha-asset/04_docs/pages/db/select.vue
@@ -2,6 +2,78 @@
   <div>
     <AppBreadCrumbs :items="breadcrumbs" />
     <AppContentsTitle title="検索" />
+    <v-row justify="center">
+      <v-col class="text-left" cols="12" sm="8" md="10">
+        <v-alert border="left" type="info" text elevation="2" dismissible
+          >簡易的にDBを検索するSQLを作成するツール。<br />以下に当たる部分を指定し作成ボタンでSQLを作成
+          <ul>
+            <li>テーブル名</li>
+            <li>WHERE句</li>
+          </ul></v-alert
+        >
+      </v-col>
+    </v-row>
+
+    <v-form ref="sqlForm">
+      <v-row>
+        <v-col cols="1" sm="1" md="1"></v-col>
+        <v-col class="text-left" cols="5" sm="5" md="5">
+          <v-select
+            :items="tables"
+            return-object
+            clearable
+            label="検索対象テーブル名"
+            item-text="logicalName"
+            item-value="physicalName"
+            v-model="selectedTable"
+            @input="setColumns"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row v-if="selectedTable != null">
+        <v-col cols="1" sm="1" md="1"></v-col>
+        <v-col class="text-left" cols="5" sm="5" md="5">
+          <v-select
+            :items="columns"
+            return-object
+            clearable
+            label="検索対象カラム名"
+            item-text="logicalName"
+            item-value="physicalName"
+            v-model="selectedColumn"
+          ></v-select>
+        </v-col>
+        <v-col class="text-left" cols="5" sm="5" md="5">
+          <v-text-field
+            label="検索対象カラムの値"
+            clearable
+            v-model="selectedColumnValue"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row
+        v-if="
+          selectedTable != null &&
+          selectedColumn != null &&
+          selectedColumnValue != null
+        "
+      >
+        <v-col cols="1" sm="1" md="1"></v-col>
+        <v-col class="text-left" cols="5" sm="5" md="5">
+          <v-btn color="info" @click="createSql">SQL生成</v-btn>
+        </v-col>
+      </v-row>
+      <v-row v-if="sql != null">
+        <v-col cols="1" sm="1" md="1"></v-col>
+        <v-col class="text-left" cols="5" sm="5" md="5">
+          <v-textarea
+            label="SQL"
+            v-model="sql"
+            readonly
+          ></v-textarea>
+        </v-col>
+      </v-row>
+    </v-form>
   </div>
 </template>
 
@@ -35,11 +107,31 @@ export default {
           href: "/db/select",
         },
       ],
-    }
-  }
+      tables: this.$store.state.db.tables,
+      columns: [],
+      selectedTable: null,
+      selectedColumn: null,
+      selectedColumnValue: null,
+      sql: null,
+    };
+  },
+  methods: {
+    setColumns: function () {
+      this.columns = this.selectedTable.columns;
+    },
+    createSql: function () {
+      this.sql =
+        "SELECT * FROM " +
+        this.selectedTable.physicalName +
+        " WHERE " +
+        this.selectedColumn.physicalName +
+        " = '" +
+        this.selectedColumnValue +
+        "';";
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 </style>
