d8b0ad1da74d8f18042d6551b1f39e0b221cbdfd
kohei-okazaki <kou1210hei@gmail.com>
2021-03-21 18:13:44 +0900

    no message

---
+++ ha-build/shell/ha-root-front-npm-build-stage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+########################################
+#
+# root front npm run build stage
+#
+########################################
+
+# ha-root/frontまで移動
+cd ../../ha-root/front
+
+# build
+npm run build:stg
---
+++ ha-build/shell/ha-root-front-npm-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+########################################
+#
+# root front npm run build
+#
+########################################
+
+# ha-root/frontまで移動
+cd ../../ha-root/front
+
+# build
+npm run build
