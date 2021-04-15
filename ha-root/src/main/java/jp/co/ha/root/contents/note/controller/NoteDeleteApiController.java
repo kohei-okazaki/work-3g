f714812613579707d1ba44bd20e51f70fa34e479
kohei-okazaki <kou1210hei@gmail.com>
2021-04-15 19:39:08 +0900

    削除処理実装

--- ha-root/src/main/java/jp/co/ha/root/contents/note/controller/NoteDeleteApiController.java
+++ ha-root/src/main/java/jp/co/ha/root/contents/note/controller/NoteDeleteApiController.java
@@ -2,11 +2,13 @@ package jp.co.ha.root.contents.note.controller;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import jp.co.ha.business.db.crud.delete.RootUserNoteInfoDeleteService;
 import jp.co.ha.common.exception.BaseException;
 import jp.co.ha.root.base.BaseRootApiController;
 import jp.co.ha.root.contents.news.request.NewsDeleteApiRequest;
@@ -21,6 +23,10 @@ import jp.co.ha.root.contents.note.response.NoteDeleteApiResponse;
 @RestController
 public class NoteDeleteApiController
         extends BaseRootApiController<NoteDeleteApiRequest, NoteDeleteApiResponse> {
+
+    /** 管理者サイトユーザメモ情報削除サービス */
+    @Autowired
+    private RootUserNoteInfoDeleteService deleteService;
 
     /**
      * メモ情報削除処理
@@ -37,6 +43,14 @@ public class NoteDeleteApiController
     public NoteDeleteApiResponse delete(
             @PathVariable(name = "seq_root_user_note_info_id", required = false) Optional<String> seqRootUserNoteInfoId,
             NewsDeleteApiRequest request) throws BaseException {
+
+        if (seqRootUserNoteInfoId == null || !seqRootUserNoteInfoId.isPresent()) {
+            // 未指定の場合
+            return getErrorResponse("seq_root_user_note_info_id is required");
+        }
+
+        // メモ情報を削除
+        deleteService.delete(Long.valueOf(seqRootUserNoteInfoId.get()));
 
         NoteDeleteApiResponse response = getSuccessResponse();
         return response;
