3d5c9e8e08bd2b5e867779f2ca4762459f85ab99
kohei-okazaki <kou1210hei@gmail.com>
2021-05-03 16:23:43 +0900

    モジュール更新

--- ha-root/src/main/java/jp/co/ha/root/base/BaseRootApiRequest.java
+++ ha-root/src/main/java/jp/co/ha/root/base/BaseRootApiRequest.java
@@ -1,13 +1,10 @@
 package jp.co.ha.root.base;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 /**
  * RootAPI基底リクエストクラス
  *
  * @version 1.0.0
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class BaseRootApiRequest extends JsonEntity {
 
 }
