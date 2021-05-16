9d19e63b064664fc40c8de1c1f6f9cc9a02b6ce8
kohei-okazaki <kou1210hei@gmail.com>
2021-05-16 17:32:43 +0900

    リファクタリング

--- ha-db/src/test/resources/logback.groovy
+++ ha-db/src/test/resources/logback.groovy
@@ -32,10 +32,10 @@ appender("STDOUT", ConsoleAppender) {
 // DEBUGログの出力設定
 appender("DEBUG_FILE", RollingFileAppender) {
 
-  file = "${FILE_PATH}/batch-debug.log"
+  file = "${FILE_PATH}/db-debug.log"
 
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "batch-debug_%d{yyyy-MM-dd}.log"
+    fileNamePattern = "db-debug_%d{yyyy-MM-dd}.log"
     maxHistory = 30
   }
 
@@ -55,10 +55,10 @@ appender("DEBUG_FILE", RollingFileAppender) {
 // INFOログの出力設定
 appender("INFO_FILE", RollingFileAppender) {
 
-  file = "${FILE_PATH}/batch-info.log"
+  file = "${FILE_PATH}/db-info.log"
 
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "batch-info_%d{yyyy-MM-dd}.log"
+    fileNamePattern = "db-info_%d{yyyy-MM-dd}.log"
     maxHistory = 30
   }
 
@@ -78,10 +78,10 @@ appender("INFO_FILE", RollingFileAppender) {
 // WARNログの出力する設定
 appender("WARN_FILE", RollingFileAppender) {
 
-  file = "${FILE_PATH}/batch-warn.log"
+  file = "${FILE_PATH}/db-warn.log"
 
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "batch-warn_%d{yyyy-MM-dd}.log"
+    fileNamePattern = "db-warn_%d{yyyy-MM-dd}.log"
     maxHistory = 30
   }
 
@@ -100,10 +100,10 @@ appender("WARN_FILE", RollingFileAppender) {
 // ERRORログの出力する設定
 appender("ERROR_FILE", RollingFileAppender) {
 
-  file = "${FILE_PATH}/batch-error.log"
+  file = "${FILE_PATH}/db-error.log"
 
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "batch-error_%d{yyyy-MM-dd}.log"
+    fileNamePattern = "db-error_%d{yyyy-MM-dd}.log"
     maxHistory = 30
   }
 
