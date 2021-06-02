48805e15055c23d776b67a013d3c7540d3603a3a
kohei-okazaki <kou1210hei@gmail.com>
2021-06-02 20:51:31 +0900

    no message

--- ha-batch/src/main/java/jp/co/ha/batch/listener/BatchJobListener.java
+++ ha-batch/src/main/java/jp/co/ha/batch/listener/BatchJobListener.java
@@ -1,5 +1,7 @@
 package jp.co.ha.batch.listener;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.Map.Entry;
 import java.util.StringJoiner;
 
@@ -8,11 +10,16 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 import jp.co.ha.business.api.slack.SlackApiComponent;
 import jp.co.ha.business.api.slack.SlackApiComponent.ContentType;
+import jp.co.ha.common.exception.BaseAppError;
+import jp.co.ha.common.exception.BaseErrorCode;
 import jp.co.ha.common.exception.BaseException;
+import jp.co.ha.common.exception.CommonErrorCode;
+import jp.co.ha.common.exception.SystemException;
 import jp.co.ha.common.exception.SystemRuntimeException;
 import jp.co.ha.common.log.Logger;
 import jp.co.ha.common.log.LoggerFactory;
@@ -31,6 +38,9 @@ public class BatchJobListener extends JobExecutionListenerSupport {
     /** {@linkplain SlackApiComponent} */
     @Autowired
     private SlackApiComponent slackApiComponent;
+    /** {@linkplain MessageSource} */
+    @Autowired
+    protected MessageSource messageSource;
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
@@ -52,11 +62,39 @@ public class BatchJobListener extends JobExecutionListenerSupport {
         try {
             String message = "name=" + jobExecution.getJobInstance().getJobName()
                     + ", status=" + jobExecution.getStatus();
-            if (BatchStatus.COMPLETED == jobExecution.getStatus()) {
-                // 正常終了している場合
-                slackApiComponent.send(ContentType.BATCH, message);
+            if (BatchStatus.FAILED == jobExecution.getStatus()) {
+                List<Throwable> exceptions = jobExecution.getAllFailureExceptions();
+                for (Throwable t : exceptions) {
+                    BaseAppError error = null;
+                    if (t instanceof BaseAppError) {
+                        error = (BaseAppError) t;
+                    } else {
+                        BaseErrorCode errorCode = CommonErrorCode.UNEXPECTED_ERROR;
+                        String detail = messageSource.getMessage(
+                                errorCode.getOuterErrorCode(), null,
+                                Locale.getDefault());
+                        error = new SystemException(errorCode, detail);
+                    }
+
+                    String errorMessage = new StringBuilder()
+                            .append(messageSource.getMessage(
+                                    error.getErrorCode().getOuterErrorCode(), null,
+                                    Locale.getDefault()))
+                            .append("(")
+                            .append(error.getErrorCode().getOuterErrorCode())
+                            .append(")")
+                            .toString();
+                    message += (", error_message=" + errorMessage);
+                    slackApiComponent.send(ContentType.BATCH,
+                            message + ", error_message=" + errorMessage);
+                }
             }
+
+            // Slackに通知
+            slackApiComponent.send(ContentType.BATCH, message);
+            // ログ出力
             LOG.debug("end JOB. " + message);
+
         } catch (BaseException e) {
             throw new SystemRuntimeException(e);
         }
