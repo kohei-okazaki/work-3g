f8bd0b7daf223c7d690cd1ef0afbdc83aacb9457
kohei-okazaki <kou1210hei@gmail.com>
2025-07-13 15:49:02 +0900

    妥当性チェック処理を強化

---
+++ ha-common/src/main/java/jp/co/ha/common/validator/annotation/Future.java
@@ -0,0 +1,37 @@
+package jp.co.ha.common.validator.annotation;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import jp.co.ha.common.validator.FutureValidator;
+
+/**
+ * 未来日妥当性チェックアノテーション
+ *
+ * @see jp.co.ha.common.validator.FutureValidator
+ * @version 1.0.0
+ */
+@Inherited
+@Documented
+@Retention(RUNTIME)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Constraint(validatedBy = FutureValidator.class)
+public @interface Future {
+
+    /** message */
+    String message() default "";
+
+    /** groups */
+    Class<?>[] groups() default {};
+
+    /** payload */
+    Class<? extends Payload>[] payload() default {};
+}
