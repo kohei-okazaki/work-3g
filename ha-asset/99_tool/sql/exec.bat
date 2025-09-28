007c07bbdaf30b8d1c3fc053c8a19c22bcb4410c
kohei-okazaki <kou1210hei@gmail.com>
2025-09-28 16:11:25 +0900

    軽微な修正

--- ha-asset/99_tool/sql/exec.bat
+++ ha-asset/99_tool/sql/exec.bat
@@ -3,19 +3,19 @@
 rem PCにインストールされているMySQLへSQLを流すbat.
 rem 同階層に配置しているexec.sqlを実行する
 rem 実行結果は標準出力する
-rem ユーザ        : root
-rem パスワード    : admin
+rem ユーザ        : app_user
+rem パスワード    : 3w4tamudnxgr4
 rem データベース  : work3g
 rem ARG1          : ログ出力先
-rem                            "console"を指定した場合、ターミナル上にSQLと処理結果が出力される
-rem                            未指定の場合、本batと同階層に"result.log"としてSQLと処理結果が出力される
+rem                     "console"を指定した場合、ターミナル上にSQLと処理結果が出力される
+rem                     未指定の場合、本batと同階層に"result.log"としてSQLと処理結果が出力される
 
 cls
 
 rem ユーザ
-SET USER=root
+SET USER=app_user
 rem パスワード
-SET PASSWORD=admin
+SET PASSWORD=3w4tamudnxgr4
 rem 接続先データベース
 SET DATABASE=work3g
 rem 日付
@@ -26,7 +26,7 @@ SET S_TIME=%time2:~0,2%%time2:~3,2%%time2:~6,2%
 rem ファイル名
 SET FILE_NAME=%S_DATE%%S_TIME%.log
 
-IF "%1" == console (
+IF "%1" == "console" (
   echo console
   mysql -u %USER% -D %DATABASE% -p%PASSWORD% -v < exec.sql 2>> error.log
 ) ELSE (
