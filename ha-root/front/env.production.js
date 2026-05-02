8e9ebc9e1ff8c368d166022d0d53e31250ff7a10
okazaki <kou1210hei@gmail.com>
2026-05-03 02:58:02 +0900

    https://github.com/kohei-okazaki/work-3g/issues/1220
    - ha-root/frontのCIを追加。
    - 本番の設定ファイルが配置されていたので削除（まだサーバとかも作っていないので問題なしだが、一部設定ファイルだけ作成されているのも統一感がない。）

--- ha-root/front/env.production.js
+++
@@ -1,9 +0,0 @@
-const apiBaseURL =
-  process.env.API_BASE_URL ||
-  process.env.api_base_url ||
-  "http://localhost:8082/api/root/";
-
-module.exports = {
-  name: "production",
-  api_base_url: apiBaseURL,
-};
